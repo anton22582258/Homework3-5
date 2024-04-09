2bbe56c57efd3e99a253ed98ddf146fca22cbfe2
Anton <anton22582258@gmail.com>
2024-04-09 19:57:22 +0300

    Homework3-5

---
+++ src/main/java/ru/hogwarts/school4/School4Application.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class School4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(School4Application.class, args);
+	}
+
+}
